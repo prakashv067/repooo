01c738ecc82256980bc3b8b6e172d4fc2175a6c3
91908 <91908@Prakash>
2024-07-30 08:52:20 +0530

    porject3.0

--- Project1/src/Main2.java
+++ Project1/src/Main2.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("Main2");
 	System.out.println("update");
 	System.out.println("main update2.0");
+	System.out.println("main project3.0");
 }
 }
